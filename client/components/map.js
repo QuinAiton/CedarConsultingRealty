fd9b9770c13a05889e320794281191a6eb6b4bc8
quinaiton <quinaiton@hotmail.com>
2021-04-06 23:26:32 -0700

    Adds Listing Components

--- client/components/map.js
+++ client/components/map.js
@@ -4,16 +4,17 @@ import ReactMapGL from 'react-map-gl';
 
 const map = () => {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
+    width: '50vw',
+    height: '100vh',
+    latitude: 49.06245087463475,
+    longitude: -123.46848129153817,
     zoom: 8,
   });
 
   return (
     <ReactMapGL
       {...viewport}
+      mapStyle='mapbox://styles/mapbox/streets-v9'
       mapboxApiAccessToken={process.env.NEXT_MAPBOX_TOKEN}
       onViewportChange={(viewport) => setViewport(viewport)}
     />
